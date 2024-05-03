172479838dd26121bc592b23a5c74a9d72466206
Vladimir Strelec <vovik050github@gmail.com>
2024-05-03 18:51:44 +0200

    Change User Model and Users client numer

--- djsite/project/project/account/models.py
+++ djsite/project/project/account/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 
+class ShopUser(AbstractBaseUser):
+    username = models.CharField(max_length=6, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"Client numer: {self.username} Email: {self.email}"
--- djsite/project/project/account/views.py
+++ djsite/project/project/account/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import RegisterUserForm
+from .models import ShopUser
 
 
 def client_numer_generator(size=6, chars=string.ascii_uppercase + string.digits):
@@ -18,18 +19,21 @@ def client_numer_generator(size=6, chars=string.ascii_uppercase + string.digits)
 #     template_name = 'register-user.html'
 #     success_url = reverse_lazy('home')
 
+
 def create_user(request):
     if request.method == 'POST':
-        form = RegisterUserForm(request.POST, request.FILES)
+        form = RegisterUserForm(request.POST)
         client_numer = client_numer_generator()
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
         if form.is_valid() and password1 == password2:
-            user = User.objects.create_user(username=client_numer, email=email, password=password1)
+            user = ShopUser.objects.create(username=client_numer, email=email, password=password1)
+            user.set_password(password1)
             user.save()
 
             return redirect('home')
+
     form = RegisterUserForm()
     return render(request, 'register-user.html', {'form': form})
